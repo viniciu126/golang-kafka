65b3fd6ed2e7fe4ec136f240276a00cfd3bb167e
viniciu126 <viniciu.mn8@hotmail.com>
2022-01-18 10:29:53 -0300

    initial commit
    api to publish message on topic

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Shopify/sarama"
+	"github.com/gofiber/fiber/v2"
+)
+
+// ConnectProducer return a kafka broker connection or error
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	// NewSyncProducer creates a new SyncProducer using the given broker addresses and configuration.
+	conn, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUrl := []string{"host.docker.internal:9094"}
+	fmt.Println(brokersUrl)
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+
+	return nil
+}
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func createComment(c *fiber.Ctx) error {
+	// Instantiate new Message struct
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+	// convert body into bytes and send it to kafka
+	cmtInBytes, err := json.Marshal(cmt)
+	err = PushCommentToQueue("comments", cmtInBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	// Return Comment in JSON format
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": cmt,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Error creating product",
+		})
+		return err
+	}
+	return err
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+	api.Post("/comment", createComment)
+	app.Listen(":3000")
+}
